d0143261419574d50baa6c90a886baa5f0adee9c
ryanraidt <ryanraidt1026@gmail.com>
2024-12-02 11:23:39 -0600

    Id puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
   return (
     <>
       <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
